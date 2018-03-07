72837ca4e311f8dabb046f5a0fec34feef5ee4aa
phpbae <phpbae@ohjic.com>
2018-03-07 09:37:42 +0900

    import 수정.

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
